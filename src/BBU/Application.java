707f5a8eca6eed8d1dc75873cf7d65099b97a0ba
afendar <mickael.comesse@gmail.com>
2015-10-27 21:22:09 +0100

    + First commit

---
+++ src/BBU/Application.java
@@ -0,0 +1,24 @@
+package BBU;
+
+import progessbartest.Fenetre;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author portable mickael
+ */
+public class Application {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Fenetre f = new Fenetre();
+        MainMenu m = new MainMenu("Brick Breaker Universe", 1024, 768);
+    }
+}
